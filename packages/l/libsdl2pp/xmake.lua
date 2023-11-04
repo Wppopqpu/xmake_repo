059c0dc6fb8ddf85abe3a548f3b42ac115d0c36c
Wppopqpu <wppopqpu@outlook.com>
2023-11-04 10:42:32 +0800

    Update sdl2pp
    fix typo

--- packages/l/libsdl2pp/xmake.lua
+++ packages/l/libsdl2pp/xmake.lua
@@ -3,8 +3,7 @@ package("libsdl2pp")
 	set_description("C++ bindings/wrapper for SDL2 and satellite libraries")
 	set_urls("https://github.com/libSDL2pp/libSDL2pp.git")
 
-	add_deps("cmake", "libsdl", "libsdl_ttf", "libsdl_net", "libsdl_gfx",
-		"libsdl_mixer", "libsdl_image")
+	add_deps("cmake", "libsdl", "libsdl_ttf", "libsdl_mixer", "libsdl_image")
 	on_install(function(package)
 		local config = {}
 		import("package.tools.cmake").install(package, config)
