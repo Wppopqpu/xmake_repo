6664d9fe59c6e9888aa27f163030285383f368eb
Wppopqpu <wppopqpu@outlook.com>
2023-11-04 09:34:25 +0800

    add package libsdl2pp

---
+++ packages/l/libsdl2pp/xmake.lua
@@ -0,0 +1,10 @@
+package("libsdl2pp")
+	set_homepage("https://github.com/libSDL2pp/libSDL2pp")
+	set_description("C++ bindings/wrapper for SDL2 and satellite libraries")
+	set_urls("https://github.com/libSDL2pp/libSDL2pp.git")
+
+	add_deps("cmake")
+	on_install(function(package)
+		local config = {}
+		import("package.tools.cmake").install(package, config)
+	end)
