425a086938dc90e3600f549fe559b6c1f96fef3b
Wppopqpu <wppopqpu@outlook.com>
2024-05-01 08:58:01 +0800

    add package EMOCK

---
+++ packages/e/emock/xmake.lua
@@ -0,0 +1,8 @@
+package("emock")
+	set_description("EMOCK is next generation easy-to-use C++ Mock Library based on mockcpp")
+	set_license("Apache-2.0")
+	add_urls("https://github.com/ez8-co/emock.git")
+
+	on_install(function(package)
+		import"package.tools.cmake".install(package, {})
+	end)
