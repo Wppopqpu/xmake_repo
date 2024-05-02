1fa91d013b451ebd8e796ce7cace4b57d9fb4302
Wppopqpu <wppopqpu@outlook.com>
2024-05-02 14:19:26 +0800

    fix emock package

--- packages/e/emock/xmake.lua
+++ packages/e/emock/xmake.lua
@@ -2,6 +2,7 @@ package("emock")
 	set_description("EMOCK is next generation easy-to-use C++ Mock Library based on mockcpp")
 	set_license("Apache-2.0")
 	add_urls("https://github.com/ez8-co/emock.git")
+	add_deps("cmake")
 
 	on_install(function(package)
 		import"package.tools.cmake".install(package, {})
